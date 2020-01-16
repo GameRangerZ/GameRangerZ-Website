27e9c09be7f28b5c6569d9c240bea15ce2443e9f
supermopf <victor.lange1998@gmail.com>
2020-01-16 16:00:29 +0100

    Added GamerTransfer

--- resources/views/content/partner.blade.php
+++ resources/views/content/partner.blade.php
@@ -8,17 +8,35 @@
             </div>
             <div class="col-lg-3">
                 <figure class="figure">
-                    <img class="figure-img img-fluid rounded" src="{{asset("img/netcup.webp")}}" alt="Netcup Logo">
+                    <a href="https://netcup.de" title="Netcup" target="_blank" rel="noopener">
+                        <img class="figure-img img-fluid rounded" src="{{asset("img/netcup.webp")}}" alt="Netcup Logo">
+                    </a>
                     <figcaption class="figure-caption">Der Hosting Provider Netcup</figcaption>
                 </figure>
             </div>
-            <div class="col-lg-8">
+            <div class="col-lg-9">
                 <h2>Netcup</h2>
                 <p class="text">
                     Alle unsere Server sind bei dem Hostingprovider Netcup gehostet. Hier kann man recht günstig
                     qualitativ hochwertige Server bekommen. Der DDoS Schutz hat uns schon das ein oder andere mal
                     gerettet, wenn ein Script-Kiddie mal wieder unseren TeamSpeak down haben wollte.
                     Der Support ist außerdem sehr freundlich und schnell.
+                </p>
+            </div>
+            <div class="col-lg-3">
+                <figure class="figure">
+                    <a href="https://gamertransfer.com" title="Gamertransfer" target="_blank" rel="noopener">
+                        <img class="figure-img img-fluid rounded-circle" src="{{asset("img/gamertransfer.webp")}}" alt="Netcup Logo">
+                    </a>
+                    <figcaption class="figure-caption">Deutscher eSport Transfermarkt</figcaption>
+                </figure>
+            </div>
+            <div class="col-lg-9">
+                <h2>GamerTransfer</h2>
+                <p class="text">
+                    GamerTransfer ist eine bekannte LFG-Plattform, welche es Spieler ermöglicht Gruppen und Teams und Clans
+                    Tuniergegner und neue Mitspieler zu finden. Wir sind hier natürlich auch gelistet und freuen uns
+                    neue Teammitglieder zu rekrutieren.
                 </p>
             </div>
         </div>
