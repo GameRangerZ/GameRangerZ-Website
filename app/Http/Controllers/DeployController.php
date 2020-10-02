6999929c46903674e900d573a8f688107fbc411a
supermopf <victor.lange1998@gmail.com>
2020-10-03 01:12:19 +0200

    Fixed Auto Deploy
    Signed-off-by: supermopf <victor.lange1998@gmail.com>

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -15,7 +15,8 @@ class DeployController extends Controller
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
         if (hash_equals($githubHash, $localHash)) {
             $root_path = base_path();
-            $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+            $process = new Process(['sh', 'deploy.sh']);
+            $process->setWorkingDirectory($root_path);
             $process->run(function ($type, $buffer) {
                 echo $buffer;
             });
